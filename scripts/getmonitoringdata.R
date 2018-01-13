f0c2d72f3d83066ee281ff9363ad93766526be41
Emma Wiik <e.wiik@bangor.ac.uk>
2018-01-13 17:44:51 +0000

    First upload of Malham stuff

---
+++ scripts/getmonitoringdata.R
@@ -0,0 +1,47 @@
+mal <- read.csv("~/static/PhDPapers/Data/Water Chemistry My Lakes/FromLiamOReilly/sondes201516.csv", skip=1)
+mal$datetime <- paste(mal$DATE, mal$TIME)
+mal$datetime <- as.POSIXct(mal$datetime, format="%d/%m/%Y %H:%M:%S", tz="GMT")
+
+## create protocol for downloading data from the yorkshire region
+## ================================================================================
+## data availability starts at 2000
+years <- 2000:2018
+namelist <- list(paste0("http://environment.data.gov.uk/water-quality/batch/measurement?area=3-34&isComplianceSample=false&year=", 
+                        years))
+destlist <- list(paste0("../data/yorkmon",years,".csv"))
+mapply(download.file, namelist, destlist)
+
+## extract malham and join the data sets with sensible column names
+## ================================================================================
+raws <- lapply(unlist(destlist), read.csv)
+
+mals <- lapply(raws, function(x) { malhs <- grep("MALH", x$sample.samplingPoint.label)
+                                  x[malhs,]})          
+maldf <- do.call(rbind, mals)
+
+names(maldf)
+names(maldf) <- c("ID","pointID","pointIDshort","Site","DateTime","var","Variable","varID","wtf",
+                  "value","interpretation","Unit","SampleMaterial","Compliance","Purpose","Easting",
+                  "Northing")
+maldf <- droplevels(maldf)
+
+## take away columns/rows we don't want for our purposes
+maldf$SampleMaterial[-grep("WATER", maldf$SampleMaterial)] # some uncoded and sewage effluent..!
+maldf <- maldf[grep("WATER", maldf$SampleMaterial),] # some uncoded and sewage effluent..!
+
+maldf <- maldf[,c("Site","DateTime","var","Variable","value", "Unit","Easting", "Northing")]
+
+
+## make time sensible
+maldf$DateTime <- gsub(x=maldf$DateTime, pattern = "T", replacement = " ") 
+maldf$DateTime <- as.POSIXct(maldf$DateTime, format = "%Y-%m-%d %H:%M:%S", tz = "Europe/London")
+
+maldf$Year <- as.numeric(format(maldf$DateTime, "%Y"))
+maldf$Month <- as.numeric(format(maldf$DateTime, "%m"))
+maldf$DOY <- as.numeric(strftime(maldf$DateTime, format = "%j"))
+maldf <- transform(maldf, Time = as.numeric(format(DateTime, "%H")) +
+                        as.numeric(format(DateTime, "%M"))/60)
+str(maldf)
+
+## save the data for analysis
+saveRDS(maldf, "../data/malham.rds")
