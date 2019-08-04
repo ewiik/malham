32373d644f866033fc4cccbce02e3fbbab4520f9
Emma Wiik <e.wiik@bangor.ac.uk>
2019-08-04 14:09:34 +0100

    renamed malham rds to reflect data source

--- scripts/get-EA-monitoringdata.R
+++ scripts/get-EA-monitoringdata.R
@@ -1,15 +1,24 @@
 ## create protocol for downloading data from the yorkshire region
 ## ================================================================================
-## data availability starts at 2000, this is the generic access url:
-years <- 2000:2018
+## data availability starts at 2000, this is the generic access url;
+##    download only those that don't already exist in files
+years <- 2000:2019
 namelist <- list(paste0("http://environment.data.gov.uk/water-quality/batch/measurement?area=3-34&isComplianceSample=false&year=", 
                         years))
-destlist <- list(paste0("../data/yorkmon",years,".csv"))
-mapply(download.file, namelist, destlist)
+namelist <- paste0("http://environment.data.gov.uk/water-quality/batch/measurement?area=3-34&isComplianceSample=false&year=", 
+                        years)
+
+destlist <- list(paste0("../dat-orig/EA-online/yorkmon",years,".csv"))
+destlist <- paste0("../dat-orig/EA-online/yorkmon",years,".csv")
+
+reps <-file.exists(destlist)
+want <- c(which(reps==F)[1]-1, which(reps==F)) # to protect incomplete years
+
+Map(download.file, namelist[want], destlist[want])
 
 ## extract malham and join the data sets with sensible column names
 ## ================================================================================
-raws <- lapply(unlist(destlist), read.csv)
+raws <- lapply(destlist, read.csv)
 
 mals <- lapply(raws, function(x) { malhs <- grep("MALH", x$sample.samplingPoint.label)
                                   x[malhs,]})          
@@ -19,11 +28,11 @@ names(maldf)
 names(maldf) <- c("ID","pointID","pointIDshort","Site","DateTime","var","Variable","varID","wtf",
                   "value","interpretation","Unit","SampleMaterial","Compliance","Purpose","Easting",
                   "Northing")
-maldf <- droplevels(maldf)
+maldf <- droplevels(maldf) # drop unused factor levels following susetting
 
 ## take away columns/rows we don't want for our purposes
-maldf$SampleMaterial[-grep("WATER", maldf$SampleMaterial)] # some uncoded and sewage effluent..!
-maldf <- maldf[grep("WATER", maldf$SampleMaterial),] # some uncoded and sewage effluent..!
+unique(maldf$SampleMaterial[-grep("WATER", maldf$SampleMaterial)]) # some uncoded and sewage effluent..!
+maldf <- maldf[grep("WATER", maldf$SampleMaterial),] 
 
 maldf <- maldf[,c("Site","DateTime","var","Variable","value", "Unit","Easting", "Northing")]
 
@@ -40,4 +49,4 @@ maldf <- transform(maldf, Time = as.numeric(format(DateTime, "%H")) +
 str(maldf)
 
 ## save the data for analysis
-saveRDS(maldf, "../data/malham.rds")
+saveRDS(maldf, "../dat-mod/malham-EA.rds")
